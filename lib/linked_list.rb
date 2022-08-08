706190f8ed6033c849e3458627be8c2121aaba68
Zach <shaw.zacht@gmail.com>
2022-08-07 22:58:04 -0700

    commit tree b560b2a6a000d8fd076118e606d47e0607c61353

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,27 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
-  end
+    if head.nil?
+      nil
+    else
+      current = head
+      length = 0
+      while current.next_node
+        length += 1
+        current = current.next_node
+      end
+      puts length
+      current = head
+      (length - n + 1).times do 
+        current = current.next_node
+      end
+      
+      if n - 2 < length && n > 0
+      current.value
+      else 
+        nil
+      end 
+    end
+end
 
 end
